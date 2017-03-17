1fc927b0b8f7479aafc87ced95a852f135e35417
caseyyee <caseyyee.ca@gmail.com>
2017-03-17 13:25:31 -0700

    basic daydream controller support

--- client/js/index.js
+++ client/js/index.js
@@ -8,22 +8,19 @@ require('./multiuser');
 
 AFRAME.registerComponent('game', {
   init: function () {
+    var self = this;
+
     var startButton = document.querySelector("#start");
     startButton.addEventListener('click', function (e) {
       console.log('start clicked!');
     });
 
-    var gopherBoxes = Array.prototype.slice.call(document.querySelectorAll('a-entity.gopherBox'));
+    var gopherBoxes = this.gopherBoxes = Array.prototype.slice.call(document.querySelectorAll('a-entity.gopherBox'));
 
     gopherBoxes.forEach(function (gopher) {
-      gopher.children[0].addEventListener('click', function (e) {
-        var target = e.detail.target;
-        if (target.getAttribute('mixin') === 'gopher up') {
-          target.setAttribute('mixin', 'gopher');
-          console.log('hit!');
-        }
-      })
-    })
+      gopher.children[0].addEventListener('raycaster-intersected', self.handleIntraction);
+      gopher.children[0].addEventListener('click', self.handleIntraction);
+    });
 
     var popGopher = function () {
       var i = Math.floor(Math.random() * gopherBoxes.length);
@@ -38,7 +35,18 @@ AFRAME.registerComponent('game', {
       var random = Math.round(Math.random() * (1000 - 100)) + 100;
       setTimeout(randomTimeout, random);
     }
+
     randomTimeout();
+
+    this.hammer = document.querySelector('#hammer-head');
+  },
+
+  handleIntraction: function (e) {
+    var target = e.detail.target;
+    if (target.getAttribute('mixin') === 'gopher up') {
+      target.setAttribute('mixin', 'gopher');
+      console.log('hit!');
+    }
   },
 
   tick: function () {
